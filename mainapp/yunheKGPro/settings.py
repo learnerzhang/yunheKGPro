bb02ce714d6cf72875d64bb60edb6f344eb14dd8
learnerzhang <10387776+learnerzhang@users.noreply.github.com>
2025-04-17 08:59:21 +0800

    rm middle logs

--- mainapp/yunheKGPro/settings.py
+++ mainapp/yunheKGPro/settings.py
@@ -287,7 +287,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'logapp.middleware.LoggingMiddleware'
 ]
 
 REST_FRAMEWORK = {
